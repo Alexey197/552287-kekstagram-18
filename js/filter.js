50489f928ca2bdea842aecf22ce0c0460a3ad283
Alexey197 <margadonnn3@gmail.com>
2019-11-09 15:51:53 +0300

    filters improvements

--- js/filter.js
+++ js/filter.js
@@ -3,7 +3,8 @@
 (function () {
   var PICTURES_AMOUNT = 10;
   var DEBOUNCE_INTERVAL = 500;
-  var FILTER_TYPE = 'filter-popular';
+  var ACTIVE_CLASS = 'img-filters__button--active';
+  var filterType = 'filter-popular';
   var buttonForm = document.querySelector('.img-filters__form');
   var activeButton = buttonForm.querySelector('.img-filters__button--active');
 
@@ -39,15 +40,19 @@
   };
 
   var getFilteredArr = function (arr) {
-    var filteredArr = filters[FILTER_TYPE](arr);
+    var filteredArr = filters[filterType](arr);
     return filteredArr;
   };
 
+  var changeButtonActivity = function (evt) {
+    activeButton.classList.remove(ACTIVE_CLASS);
+    activeButton = evt.target;
+    activeButton.classList.add(ACTIVE_CLASS);
+  };
+
   var filtersButtonClickHandler = function (evt) {
-    FILTER_TYPE = evt.target.id;
-    activeButton.classList.remove('img-filters__button--active');
-    var currentActiveButton = evt.target;
-    currentActiveButton.classList.add('img-filters__button--active');
+    filterType = evt.target.id;
+    changeButtonActivity(evt);
     window.gallery.update();
   };
 
